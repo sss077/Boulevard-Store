156f6949ac7f085f9ee7a15c0ecd755326c30db9
Ismoilov Sirojiddin <provebsiroj@gmail.com>
2023-01-22 16:37:49 +0500

    finished O.O

--- js/script.js
+++ js/script.js
@@ -1,52 +1,56 @@
+// text animation
 let i = 0;
 let txt = " AirPods Pro"
 let speed = 100;
 
 function typeWriter() {
-    document.getElementById("title").innerHTML += txt.charAt(i);
-    i++;
-    if (i < txt.length) {
-        setTimeout(typeWriter, speed);
-    }
+  document.getElementById("title").innerHTML += txt.charAt(i);
+  i++;
+  if (i < txt.length) {
+    setTimeout(typeWriter, speed);
+  }
 }
 typeWriter()
-
+// btn animation
 let headerBtn = document.querySelector('.header__link');
 function linkAnime() {
   if (headerBtn.classList.contains('active')) {
     headerBtn.classList.remove('active')
   }
-  else{
+  else {
     headerBtn.classList.add('active')
   }
 }
 setTimeout(() => {
-    linkAnime()
- }, 1500);
+  linkAnime()
+}, 1500);
 
-//  let cardsProduct = document.querySelector('#card1');
-//  console.log((cardsProduct.offsetTop - cardsProduct.offsetHeight) / 2.5);
-// window.addEventListener('scroll', function () { 
-//   if(window.scrollY >= cardsProduct.offsetTop - cardsProduct.offsetHeight * 2 )    
-//   {
-//     console.log('yes');
-//   }
-//   else{
-//     console.log('no');
-//   }
-//  })
-let cards = document.querySelectorAll('.right');
- cards.forEach(card => {
+//  section animation
+let upCards = document.querySelectorAll('.up');
+upCards.forEach(card => {
   const speed = card.getAttribute('data-speed')
   card.style.transition = `${speed}ms`
-  window.addEventListener('scroll', function () { 
-    if (window.scrollY >= card.offsetTop - card.offsetHeight * 2.1){
-    // console.log('yes');
-    card.classList.add('active')
+  window.addEventListener('scroll', function () {
+    if (window.scrollY >= card.offsetTop - card.offsetHeight * 2.1) {
+      card.classList.add('active')
     }
-    else{
-     card.classList.remove('active')
-    //  console.log('no');
+    else {
+      card.classList.remove('active')
     }
-   })
- });
+  })
+});
+
+//  phones animation
+let rightCards = document.querySelectorAll('.right');
+rightCards.forEach(rights => {
+  const speed = rights.getAttribute('data-speed')
+  rights.style.transition = `${speed}ms`
+  window.addEventListener('scroll', function () {
+    if (window.scrollY >= rights.offsetTop - rights.offsetHeight * 2.7) {
+      rights.classList.add('active')
+    }
+    else {
+      rights.classList.remove('active')
+    }
+  })
+});
